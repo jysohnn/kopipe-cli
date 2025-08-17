5b7218a51c69418e9c4c6a0400cd1de72ca82fe2
jysohnn <jysohnn@gmail.com>
2025-08-17 18:24:52 +0900

    refactor: 코드 간소화

--- src/main/kotlin/io/github/jysohnn/kopipe/cli/Main.kt
+++ src/main/kotlin/io/github/jysohnn/kopipe/cli/Main.kt
@@ -1,21 +1,29 @@
 package io.github.jysohnn.kopipe.cli
 
+import io.github.jysohnn.kopipe.context.Context
+import io.github.jysohnn.kopipe.context.Message
+import io.github.jysohnn.kopipe.context.Role
 import io.github.jysohnn.kopipe.knowledge.OpenAIEmbeddingVectorStore
 import io.github.jysohnn.kopipe.pipe.languagemodel.ContextAwareLanguageModel
 import io.github.jysohnn.kopipe.pipe.languagemodel.OpenAILanguageModel
+import io.github.jysohnn.kopipe.tool.Tool
 import io.github.jysohnn.kopipe.tool.ToolSelector
 import io.github.jysohnn.kopipe.tool.shell.ShellToolBox
 import java.io.File
 
 fun main() {
-    val cli = CommandLineInterface
+    val context = Context()
+    val knowledgeContext = Context()
+    val toolContext = Context()
 
     val contextAwareLanguageModel = ContextAwareLanguageModel(
-        languageModel = OpenAILanguageModel()
+        languageModel = OpenAILanguageModel(),
+        context = context,
+        knowledgeContext = knowledgeContext,
+        toolContext = toolContext
     )
-    val knowledgeStore = OpenAIEmbeddingVectorStore(
-        isPossibleToRepeatRetrieving = false
-    )
+
+    val knowledgeStore = OpenAIEmbeddingVectorStore()
     knowledgeStore.addAll(
         knowledge = createKnowledgeOfCurrentDirectory()
     )
@@ -26,60 +34,62 @@ fun main() {
     )
 
     while (true) {
-        val userInput = cli.readUserInput()
+        val userInput = readUserInput()
         if (userInput == "/quit") break
 
         val knowledge = knowledgeStore.retrieve(
             query = userInput,
             minSimilarity = 0.8
         )
-        knowledge?.let {
-            contextAwareLanguageModel.appendKnowledgeContext(it)
-        }
+
+        knowledge?.let { knowledgeContext.append(Message(Role.KNOWLEDGE, it)) }
 
         val (tool, toolInput) = toolSelector.select(
-            context = contextAwareLanguageModel.context,
-            input = userInput
+            input = userInput,
+            context = context,
+            knowledgeContext = knowledgeContext,
+            toolContext = toolContext
         )
-        val toolResult = tool?.let {
-            cli.printToolInput(toolName = tool.name, toolInput = toolInput)
-            val answer = cli.readUserInput()
+        val toolResult = tool?.let { invokeTool(tool, toolInput) }
 
-            var toolOutput = "User refuses to run tool."
-            if (answer == "yes" || answer == "y") {
-                toolOutput = tool.invoke(input = toolInput)
-
-                cli.printToolOutput(toolOutput = toolOutput)
-            }
-
-            """|- Tool Name: ${it.name}
-               |- Tool Input: $toolInput
-               |- Tool Output: $toolOutput
-            """.trimMargin()
-        }
-
-        contextAwareLanguageModel.appendUserContext(userInput)
-        toolResult?.let { contextAwareLanguageModel.appendToolContext(it) }
+        toolResult?.let { toolContext.append(Message(Role.TOOL, it)) }
 
         val assistantOutput = contextAwareLanguageModel.execute(
             input = userInput
         )
-        cli.printAssistantOutput(assistantOutput = assistantOutput)
-
-        contextAwareLanguageModel.appendAssistantContext(assistantOutput)
+        printAssistantOutput(assistantOutput = assistantOutput)
     }
+}
+
+private fun invokeTool(tool: Tool, input: String): String {
+    printToolInput(toolName = tool.name, toolInput = input)
+    val answer = readUserInput()
+
+    var output = "User refuses to run tool."
+    if (answer == "yes" || answer == "y") {
+        output = tool.invoke(input = input)
+        printToolOutput(toolOutput = output)
+    }
+
+    return """
+            |- Tool Name: ${tool.name}
+            |- Tool Input: $input
+            |- Tool Output: $output
+           """.trimMargin()
 }
 
 private fun createKnowledgeOfCurrentDirectory(): List<String> {
     val knowledge = mutableListOf<String>()
+    val maxFileSizeBytes = 1024 * 1024
+    val availableFileExtension = listOf(
+        ".txt", ".kt", ".kts", ".java", ".js", ".py", ".c", ".cpp", ".rb", ".ts",
+        ".tsx", ".jsx", ".html", ".css", ".xml", ".json", ".yaml", ".yml", ".properties"
+    )
 
     File(".").walk().forEach {
         if (!it.isFile) return@forEach
-        if (it.length() > 1024 * 1024) return@forEach
-        if (!listOf(
-                ".txt", ".kt", ".js", ".java", ".kts", ".json", ".yaml", ".yml", ".properties"
-            ).any { extension -> it.name.endsWith(extension) }
-        ) return@forEach
+        if (it.length() > maxFileSizeBytes) return@forEach
+        if (!availableFileExtension.any { extension -> it.name.endsWith(extension) }) return@forEach
 
         knowledge.add(
             """
@@ -91,4 +101,50 @@ private fun createKnowledgeOfCurrentDirectory(): List<String> {
     }
 
     return knowledge
+}
+
+private const val RESET_TEXT_COLOR = "\u001B[0m"
+private const val GREEN_TEXT_COLOR = "\u001B[32m"
+private const val BLUE_TEXT_COLOR = "\u001B[34m"
+private const val YELLOW_TEXT_COLOR = "\u001B[33m"
+
+private fun readUserInput(): String {
+    print(
+        """
+            |$GREEN_TEXT_COLOR
+            |[${Role.USER}]
+            |$RESET_TEXT_COLOR""".trimMargin()
+    )
+    return readln()
+}
+
+private fun printToolInput(toolName: String, toolInput: String) {
+    print(
+        """
+            |${YELLOW_TEXT_COLOR}
+            |[${Role.TOOL}]
+            |Do you want to use the $toolName tool with the following request? (yes/no)
+            |$toolInput
+            |$RESET_TEXT_COLOR""".trimMargin()
+    )
+}
+
+private fun printToolOutput(toolOutput: String) {
+    print(
+        """
+            |${YELLOW_TEXT_COLOR}
+            |[${Role.TOOL}]
+            |$toolOutput
+            |$RESET_TEXT_COLOR""".trimMargin()
+    )
+}
+
+private fun printAssistantOutput(assistantOutput: String) {
+    print(
+        """
+            |${BLUE_TEXT_COLOR}
+            |[${Role.ASSISTANT}]
+            |$assistantOutput
+            |$RESET_TEXT_COLOR""".trimMargin()
+    )
 }
